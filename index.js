d9dc0a81aa17a1f1e63707c4bc9d87ec4e741c32
arlsurya <suryaaryal204@gmail.com>
2024-04-06 21:02:14 +0545

    Generate Certificate with openSSL

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
 const express = require('express');
-const http = require('http');
+const https = require('https'); 
 const fs = require('fs');
 const path = require('path');
 
 const app = express();
 
-
-const sslServer = http.createServer({
-    key: '',
-    cert: ''
-
+const sslServer = https.createServer({
+    key: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
+    cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem'))
 }, app);
 
-app.get('/',(req,res,next)=>{
-    res.send('Welcome to SSL server')
-})
+app.get('/', (req, res, next) => {
+    res.send('Welcome to SSL server');
+});
 
 sslServer.listen(3000, () => {
-    console.log('ssl server is running on port 3000');
-})
-
+    console.log('SSL server is running on port 3000');
+});
